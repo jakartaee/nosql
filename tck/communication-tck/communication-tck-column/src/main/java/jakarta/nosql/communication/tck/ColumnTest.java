be249c2301df958fd53aaf1ce80a868fcbd55262
otaviojava <otaviopolianasantana@gmail.com>
2020-02-16 15:15:52 -0300

    creates column test

--- tck/communication-tck/communication-tck-column/src/main/java/jakarta/nosql/communication/tck/ColumnTest.java
+++ tck/communication-tck/communication-tck-column/src/main/java/jakarta/nosql/communication/tck/ColumnTest.java
@@ -24,11 +24,10 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class ColumnTest {
-
-
 
     private static final Value DEFAULT_VALUE = Value.of(12);
 
