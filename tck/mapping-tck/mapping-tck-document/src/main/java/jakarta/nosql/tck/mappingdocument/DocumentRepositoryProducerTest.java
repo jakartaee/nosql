3a39ff91c8edfb7cb50315f18ec6a9675a3c52ec
otaviojava <otaviopolianasantana@gmail.com>
2020-03-01 20:34:10 -0300

    adds modules to document mapping

---
+++ tck/mapping-tck/mapping-tck-document/src/main/java/jakarta/nosql/tck/mappingdocument/DocumentRepositoryProducerTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2020 Otavio Santana and others
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License v. 2.0 are satisfied: GNU General Public License v2.0
+ * w/Classpath exception which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ */
+package jakarta.nosql.tck.mappingdocument;
+
+import jakarta.nosql.document.DocumentCollectionManager;
+import jakarta.nosql.mapping.document.DocumentRepositoryProducer;
+import jakarta.nosql.mapping.document.DocumentTemplate;
+import jakarta.nosql.tck.entities.PersonRepository;
+import jakarta.nosql.tck.test.CDIExtension;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import javax.inject.Inject;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@CDIExtension
+class DocumentRepositoryProducerTest {
+
+    @Inject
+    private DocumentRepositoryProducer producer;
+
+
+    @Test
+    public void shouldCreateFromManager() {
+        DocumentCollectionManager manager= Mockito.mock(DocumentCollectionManager.class);
+        PersonRepository personRepository = producer.get(PersonRepository.class, manager);
+        assertNotNull(personRepository);
+    }
+
+
+    @Test
+    public void shouldCreateFromTemplate() {
+        DocumentTemplate template= Mockito.mock(DocumentTemplate.class);
+        PersonRepository personRepository = producer.get(PersonRepository.class, template);
+        assertNotNull(personRepository);
+    }
+}
