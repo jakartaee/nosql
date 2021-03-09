75faf6c75d04785aefe162ba9e4fdef7aae2c0e0
Otavio Santana <otaviopolianasantana@gmail.com>
2021-03-09 20:37:47 +0000

    improves javadoc to column family

--- api/mapping/mapping-column/src/main/java/jakarta/nosql/mapping/column/ColumnTemplate.java
+++ api/mapping/mapping-column/src/main/java/jakarta/nosql/mapping/column/ColumnTemplate.java
@@ -28,167 +28,13 @@ import java.util.Optional;
 import java.util.stream.Stream;
 
 /**
- * This interface that represents the common operation between an entity
- * and {@link jakarta.nosql.column.ColumnEntity}
+ * ColumnTemplate is a helper class that increases productivity when performing common Column Family operations.
+ * Includes integrated object mapping between documents and POJOs.
+ * It represents the common operation between an entity and {@link jakarta.nosql.column.ColumnEntity}
  *
  * @see jakarta.nosql.column.ColumnFamilyManager
  */
 public interface ColumnTemplate {
-
-    /**
-     * Inserts entity
-     *
-     * @param entity entity to be saved
-     * @param <T>    the instance type
-     * @return the entity saved
-     * @throws NullPointerException when entity is null
-     */
-    <T> T insert(T entity);
-
-
-    /**
-     * Inserts entity with time to live
-     *
-     * @param entity entity to be saved
-     * @param ttl    the time to live
-     * @param <T>    the instance type
-     * @return the entity saved
-     */
-    <T> T insert(T entity, Duration ttl);
-
-
-    /**
-     * Inserts entity, by default it's just run for each saving using
-     * {@link ColumnTemplate#insert(Object)}},
-     * each NoSQL vendor might replace to a more appropriate one.
-     *
-     * @param entities entities to be saved
-     * @param <T>      the instance type
-     * @return the entity saved
-     * @throws NullPointerException when entities is null
-     */
-    <T> Iterable<T> insert(Iterable<T> entities);
-
-    /**
-     * Inserts entities collection entity with time to live, by default it's just run for each saving using
-     * {@link ColumnTemplate#insert(Object, Duration)},
-     * each NoSQL vendor might replace to a more appropriate one.
-     *
-     * @param entities entities to be saved
-     * @param <T>      the instance type
-     * @param ttl      time to live
-     * @return the entity saved
-     * @throws NullPointerException when entities is null
-     */
-    <T> Iterable<T> insert(Iterable<T> entities, Duration ttl);
-
-
-    /**
-     * Updates a entity
-     *
-     * @param entity entity to be updated
-     * @param <T>    the instance type
-     * @return the entity updated
-     * @throws NullPointerException when entity is null
-     */
-    <T> T update(T entity);
-
-
-    /**
-     * Saves entity, by default it's just run for each saving using
-     * {@link ColumnTemplate#update(Object)}},
-     * each NoSQL vendor might replace to a more appropriate one.
-     *
-     * @param entities entities to be updated
-     * @param <T>      the instance type
-     * @return the entity saved
-     * @throws NullPointerException when entities is null
-     */
-    <T> Iterable<T> update(Iterable<T> entities);
-
-    /**
-     * Deletes an entity
-     *
-     * @param query query to delete an entity
-     * @throws NullPointerException when query is null
-     */
-    void delete(ColumnDeleteQuery query);
-
-
-    /**
-     * Finds entities from query
-     *
-     * @param query - query to figure out entities
-     * @param <T>   the instance type
-     * @return entities found by query
-     * @throws NullPointerException when query is null
-     */
-    <T> Stream<T> select(ColumnQuery query);
-
-    /**
-     * Finds entities from query using pagination
-     *
-     * @param query - query to figure out entities
-     * @param <T>   the instance type
-     * @return entities found by query
-     * @throws NullPointerException when query is null
-     */
-    <T> Page<T> select(ColumnQueryPagination query);
-
-    /**
-     * Executes a query then bring the result as a {@link Stream}
-     *
-     * @param query the query
-     * @param <T>   the entity type
-     * @return the result as {@link Stream}
-     * @throws NullPointerException when the query is null
-     */
-    <T> Stream<T> query(String query);
-
-    /**
-     * Executes a query then bring the result as a unique result
-     *
-     * @param query the query
-     * @param <T>   the entity type
-     * @return the result as {@link Optional}
-     * @throws NullPointerException     when the query is null
-     * @throws jakarta.nosql.NonUniqueResultException if returns more than one result
-     */
-    <T> Optional<T> singleResult(String query);
-
-    /**
-     * Creates a {@link PreparedStatement} from the query
-     *
-     * @param query the query
-     * @return a {@link PreparedStatement} instance
-     * @throws NullPointerException when the query is null
-     */
-    PreparedStatement prepare(String query);
-
-    /**
-     * Finds by Id.
-     *
-     * @param entityClass the entity class
-     * @param id          the id value
-     * @param <T>         the entity class type
-     * @param <K>        the id type
-     * @return the entity instance otherwise {@link Optional#empty()}
-     * @throws NullPointerException                   when either the entityClass or id are null
-     * @throws IdNotFoundException when the entityClass does not have the Id annotation
-     */
-    <T, K> Optional<T> find(Class<T> entityClass, K id);
-
-    /**
-     * Deletes by Id.
-     *
-     * @param entityClass the entity class
-     * @param id          the id value
-     * @param <T>         the entity class type
-     * @param <K>        the id type
-     * @throws NullPointerException                   when either the entityClass or id are null
-     * @throws IdNotFoundException when the entityClass does not have the Id annotation
-     */
-    <T, K> void delete(Class<T> entityClass, K id);
 
     /**
      * Returns the number of elements from column family
@@ -210,6 +56,34 @@ public interface ColumnTemplate {
      * @throws UnsupportedOperationException when the database dot not have support
      */
     <T> long count(Class<T> entityClass);
+
+    /**
+     * Deletes an entity
+     *
+     * @param query query to delete an entity
+     * @throws NullPointerException when query is null
+     */
+    void delete(ColumnDeleteQuery query);
+
+    /**
+     * Finds entities from query
+     *
+     * @param query - query to figure out entities
+     * @param <T>   the instance type
+     * @return entities found by query
+     * @throws NullPointerException when query is null
+     */
+    <T> Stream<T> select(ColumnQuery query);
+
+    /**
+     * Finds entities from query using pagination
+     *
+     * @param query - query to figure out entities
+     * @param <T>   the instance type
+     * @return entities found by query
+     * @throws NullPointerException when query is null
+     */
+    <T> Page<T> select(ColumnQueryPagination query);
 
     /**
      * Returns a single entity from query
