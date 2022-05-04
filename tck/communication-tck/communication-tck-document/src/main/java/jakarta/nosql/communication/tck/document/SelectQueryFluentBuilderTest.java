99572316a334f14f440d69226301683c994d1586
Otavio Santana <otaviopolianasantana@gmail.com>
2022-05-04 11:18:12 +0100

    Rename to fluent API test
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- tck/communication-tck/communication-tck-document/src/main/java/jakarta/nosql/communication/tck/document/SelectQueryFluentBuilderTest.java
+++ tck/communication-tck/communication-tck-document/src/main/java/jakarta/nosql/communication/tck/document/SelectQueryFluentBuilderTest.java
@@ -45,7 +45,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
-public class SelectQueryBuilderTest {
+public class SelectQueryFluentBuilderTest {
 
 
     @Test
