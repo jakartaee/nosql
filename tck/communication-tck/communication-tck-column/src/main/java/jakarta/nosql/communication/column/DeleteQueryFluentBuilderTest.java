56f615c27d64ee5da63ae237ab2c6b44cf4000f0
Otavio Santana <otaviopolianasantana@gmail.com>
2022-05-04 18:45:45 +0100

    Rename to DeleteQueryFluentBuilder
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- tck/communication-tck/communication-tck-column/src/main/java/jakarta/nosql/communication/column/DeleteQueryFluentBuilderTest.java
+++ tck/communication-tck/communication-tck-column/src/main/java/jakarta/nosql/communication/column/DeleteQueryFluentBuilderTest.java
@@ -38,7 +38,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
-public class DeleteQueryBuilderTest {
+public class DeleteQueryFluentBuilderTest {
 
     @Test
     public void shouldReturnErrorWhenHasNullElementInSelect() {
