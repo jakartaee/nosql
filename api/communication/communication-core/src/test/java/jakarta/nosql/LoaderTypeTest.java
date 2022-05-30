0cce50ca66283365a29f444de4ebc3757cc63b5e
Otavio Santana <otaviopolianasantana@gmail.com>
2022-05-30 10:11:15 +0100

    remove unused imports
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- api/communication/communication-core/src/test/java/jakarta/nosql/LoaderTypeTest.java
+++ api/communication/communication-core/src/test/java/jakarta/nosql/LoaderTypeTest.java
@@ -22,8 +22,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class LoaderTypeTest {
 
     @Test
