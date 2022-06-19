c82ac3bbc09c8670b8b025ca549bfe8b0a54a0bd
Otavio Santana <otaviopolianasantana@gmail.com>
2022-06-19 17:00:51 +0100

    Fix typo in column
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- api/communication/communication-column/src/main/java/module-info.java
+++ api/communication/communication-column/src/main/java/module-info.java
@@ -13,7 +13,7 @@
  *
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  */
-module jakarata.nosql.communication.column {
+module jakarta.nosql.communication.column {
     requires jakarta.nosql.communication.core;
     requires jakarta.nosql.communication.query;
 
