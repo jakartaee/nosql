c6035fedf5293ed7e9da4f797808e570ebf9a698
Otavio Santana <otaviopolianasantana@gmail.com>
2022-06-19 18:43:33 +0100

    Add tck-core module-info
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- tck/mapping-tck/mapping-tck-core/src/main/java/module-info.java
+++ tck/mapping-tck/mapping-tck-core/src/main/java/module-info.java
@@ -1,2 +1,21 @@
-module $MODULE_NAME$ {
+/*
+ * Copyright (c) 2022 Otavio Santana and others
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License v. 2.0 are satisfied: GNU General Public License v2.0
+ * w/Classpath exception which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ */
+module jakarta.nosql.mapping.tck.core {
+    requires org.junit.jupiter.api;
+    requires jakarta.nosql.mapping.core;
+    requires hamcrest.all;
+    requires jakarta.nosql.communication.core;
+    exports jakarta.nosql.tck.mapping;
 }
