f63af83d34e51ca8f8144d4fd73e6bf53a6171dc
Otavio Santana <otaviopolianasantana@gmail.com>
2022-06-19 18:06:39 +0100

    Create module to tck-driver-key-value
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- tck/driver-tck/driver-tck-key-value/src/main/java/module-info.java
+++ tck/driver-tck/driver-tck-key-value/src/main/java/module-info.java
@@ -1,2 +1,24 @@
-module $MODULE_NAME$ {
+/*
+ *  Copyright (c) 2022 Otavio Santana and others
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Eclipse Public License v. 2.0 which is available at
+ *  http://www.eclipse.org/legal/epl-2.0.
+ *
+ *  This Source Code may also be made available under the following Secondary
+ *  Licenses when the conditions for such availability set forth in the Eclipse
+ *  Public License v. 2.0 are satisfied: GNU General Public License v2.0
+ *  w/Classpath exception which is available at
+ *  https://www.gnu.org/software/classpath/license.html.
+ *
+ *  SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ */
+module jakarta.nosql.communication.driver.tck.key.value {
+    requires jakarta.nosql.communication.key.value;
+    requires jakarta.nosql.communication.core;
+    requires org.junit.jupiter.api;
+    requires hamcrest.all;
+    requires java.json.bind;
+    exports jakarta.nosql.tck.communication.driver.keyvalue;
+    opens jakarta.nosql.tck.communication.driver.keyvalue;
 }
