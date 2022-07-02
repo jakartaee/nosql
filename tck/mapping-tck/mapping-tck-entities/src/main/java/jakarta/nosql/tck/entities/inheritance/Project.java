f213cddc4149614d817c51f4f8f46262ad378c11
Otavio Santana <otaviopolianasantana@gmail.com>
2022-07-02 17:51:03 +0100

    Add Project sample
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- tck/mapping-tck/mapping-tck-entities/src/main/java/jakarta/nosql/tck/entities/inheritance/Project.java
+++ tck/mapping-tck/mapping-tck-entities/src/main/java/jakarta/nosql/tck/entities/inheritance/Project.java
@@ -15,5 +15,46 @@
 
 package jakarta.nosql.tck.entities.inheritance;
 
+import jakarta.nosql.mapping.DiscriminatorColumn;
+import jakarta.nosql.mapping.Entity;
+import jakarta.nosql.mapping.Id;
+import jakarta.nosql.mapping.Inheritance;
+
+import java.util.Objects;
+
+@Entity
+@Inheritance
+@DiscriminatorColumn("size")
 public class Project {
+
+    @Id
+    protected String name;
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Project project = (Project) o;
+        return Objects.equals(name, project.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
+    }
+
+    @Override
+    public String toString() {
+        return "Project{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
