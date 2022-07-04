5ac22e149273a4b08cac17f9e8e07e98db5b03dc
Otavio Santana <otaviopolianasantana@gmail.com>
2022-07-04 18:35:34 +0100

    Update tck mapping test
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- tck/mapping-tck/mapping-tck-document/src/main/java/jakarta/nosql/tck/mapping/document/InheritanceDocumentEntityConverterTest.java
+++ tck/mapping-tck/mapping-tck-document/src/main/java/jakarta/nosql/tck/mapping/document/InheritanceDocumentEntityConverterTest.java
@@ -101,7 +101,7 @@ public class InheritanceDocumentEntityConverterTest {
         entity.add("name", "Social Media");
         entity.add("nickname", "otaviojava");
         entity.add("createdOn",date);
-        entity.add("type", SocialMediaNotification.class.getSimpleName());
+        entity.add("dtype", SocialMediaNotification.class.getSimpleName());
         SocialMediaNotification notification = this.converter.toEntity(entity);
         assertEquals(100L, notification.getId());
         assertEquals("Social Media", notification.getName());
@@ -117,7 +117,7 @@ public class InheritanceDocumentEntityConverterTest {
         entity.add("name", "SMS Notification");
         entity.add("phone", "+351987654123");
         entity.add("createdOn", date);
-        entity.add("type", "SMS");
+        entity.add("dtype", "SMS");
         SmsNotification notification = this.converter.toEntity(entity);
         Assertions.assertEquals(100L, notification.getId());
         Assertions.assertEquals("SMS Notification", notification.getName());
@@ -133,7 +133,7 @@ public class InheritanceDocumentEntityConverterTest {
         entity.add("name", "Email Notification");
         entity.add("email", "otavio@otavio.test");
         entity.add("createdOn", date);
-        entity.add("type", "Email");
+        entity.add("dtype", "Email");
         EmailNotification notification = this.converter.toEntity(entity);
         Assertions.assertEquals(100L, notification.getId());
         Assertions.assertEquals("Email Notification", notification.getName());
@@ -208,7 +208,7 @@ public class InheritanceDocumentEntityConverterTest {
         entity.add("name", "Email Notification");
         entity.add("email", "otavio@otavio.test");
         entity.add("createdOn", date);
-        entity.add("type", "Wrong");
+        entity.add("dtype", "Wrong");
         Assertions.assertThrows(MappingException.class, ()-> this.converter.toEntity(entity));
     }
 }
