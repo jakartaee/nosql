5890042c3545fcb8e723e3a1656a18ddd82b1deb
Otavio Santana <otaviopolianasantana@gmail.com>
2022-08-07 07:37:27 +0100

    Add project TCK to Project entity with document
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- tck/mapping-tck/mapping-tck-document/src/main/java/jakarta/nosql/tck/mapping/document/InheritanceDocumentEntityConverterTest.java
+++ tck/mapping-tck/mapping-tck-document/src/main/java/jakarta/nosql/tck/mapping/document/InheritanceDocumentEntityConverterTest.java
@@ -94,6 +94,26 @@ public class InheritanceDocumentEntityConverterTest {
     }
 
     @Test
+    public void shouldConvertProject() {
+        DocumentEntity entity = DocumentEntity.of("Project");
+        entity.add("_id", "Project");
+        entity.add("size", "Project");
+        Project project = this.converter.toEntity(entity);
+        assertEquals("Project", project.getName());
+    }
+
+    @Test
+    public void shouldConvertProjectToCommunicationEntity() {
+        Project project = new Project();
+        project.setName("Large Project");
+        DocumentEntity entity = this.converter.toDocument(project);
+        assertNotNull(entity);
+        assertEquals("Project", entity.getName());
+        assertEquals(project.getName(), entity.find("_id", String.class).get());
+        assertEquals("Project", entity.find("size", String.class).get());
+    }
+
+    @Test
     public void shouldConvertDocumentEntityToSocialMedia(){
         LocalDate date = LocalDate.now();
         DocumentEntity entity = DocumentEntity.of("Notification");
