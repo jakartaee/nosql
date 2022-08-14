7c4b7b7d3d92f6e0fe18afaacc7d79301b5da550
Otavio Santana <otaviopolianasantana@gmail.com>
2022-08-14 14:03:37 +0100

    create test to DocumentEntityConverrter
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- tck/mapping-tck/mapping-tck-document/src/main/java/jakarta/nosql/tck/mapping/document/DocumentEntityConverterConstructorTest.java
+++ tck/mapping-tck/mapping-tck-document/src/main/java/jakarta/nosql/tck/mapping/document/DocumentEntityConverterConstructorTest.java
@@ -14,5 +14,45 @@
  */
 package jakarta.nosql.tck.mapping.document;
 
+import jakarta.nosql.document.DocumentEntity;
+import jakarta.nosql.mapping.document.DocumentEntityConverter;
+import jakarta.nosql.tck.entities.Money;
+import jakarta.nosql.tck.entities.constructor.Computer;
+import jakarta.nosql.tck.test.CDIExtension;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
+
+@CDIExtension
 public class DocumentEntityConverterConstructorTest {
+
+    @Inject
+    private DocumentEntityConverter converter;
+
+    @Test
+    public void shouldConverterEntityComputer() {
+        DocumentEntity communication = DocumentEntity.of("Computer");
+        communication.add("_id", 10L);
+        communication.add("name", "Dell");
+        communication.add("age", 2020);
+        communication.add("model", "Dell 2020");
+        communication.add("price", "USD 20");
+        Computer computer = this.converter.toEntity(communication);
+        Assertions.assertNotNull(computer);
+        Assertions.assertEquals(10L, computer.getId());
+        Assertions.assertEquals("Dell", computer.getName());
+        Assertions.assertEquals(2020, computer.getAge());
+        Assertions.assertEquals("Dell 2020", computer.getModel());
+        Assertions.assertEquals(Money.parse("USD 20"), computer.getPrice());
+    }
+
+    @Test
+    public void shouldConvertComputerToCommunication() {
+        Computer computer = new Computer(10L, "Dell", 2020, "Dell 2020",
+                Money.parse("USD 20"));
+        DocumentEntity communication = this.converter.toDocument(computer);
+        Assertions.assertNotNull(communication);
+
+    }
 }
