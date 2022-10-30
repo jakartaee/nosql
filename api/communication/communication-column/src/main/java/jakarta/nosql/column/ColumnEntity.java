4714696f592cf5926627cad0336cac1ac65be369
Otavio Santana <otaviopolianasantana@gmail.com>
2022-10-30 16:11:14 +0000

    Update column entity javadoc
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- api/communication/communication-column/src/main/java/jakarta/nosql/column/ColumnEntity.java
+++ api/communication/communication-column/src/main/java/jakarta/nosql/column/ColumnEntity.java
@@ -31,9 +31,6 @@ import java.util.function.Function;
 /**
  * A column family is a NoSQL object that contains columns of related data. It is a tuple (pair) that consists
  * of a key-value pair, where the key is mapped to a value that is a set of columns.
- * In analogy with relational databases, a column family is as a "table", each key-value pair being a "row".
- * Each column is a tuple (triplet) consisting of a column name, a value, and a timestamp.
- * In a relational database table, this data would be grouped together within a table with other non-related data.
  */
 public interface ColumnEntity {
 
