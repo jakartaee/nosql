0057a47e8b7102ee7550991205c24ad968293530
Otavio Santana <otaviopolianasantana@gmail.com>
2022-11-14 13:46:30 +0000

    fixes imports
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- tck/communication-tck/communication-tck-core/src/main/java/jakarta/nosql/communication/tck/SettingsTest.java
+++ tck/communication-tck/communication-tck-core/src/main/java/jakarta/nosql/communication/tck/SettingsTest.java
@@ -25,7 +25,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.function.Supplier;
 
