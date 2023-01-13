aba7ecf58bce648f456b75899aa8fe657739fb77
Otavio Santana <otaviopolianasantana@gmail.com>
2023-01-13 07:12:54 -0300

    remove api
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- api/nosql-core/src/main/java/module-info.java
+++ api/nosql-core/src/main/java/module-info.java
@@ -14,7 +14,6 @@
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  */
 module jakarta.nosql.core {
-    requires jakarta.inject;
     requires jakarta.data.api;
     exports jakarta.nosql;
 }
