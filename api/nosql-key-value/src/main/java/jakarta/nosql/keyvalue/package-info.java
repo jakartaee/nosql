97a4fbf5facf5cfcb2f3e9e8cbc2394510e4013f
Maximillian Arruda <dearrudam@gmail.com>
2023-06-12 09:26:18 -0300

    docs: enhance `jakarta.nosql.keyvalue` package javadoc
    Signed-off-by: Maximillian Arruda <dearrudam@gmail.com>

--- api/nosql-key-value/src/main/java/jakarta/nosql/keyvalue/package-info.java
+++ api/nosql-key-value/src/main/java/jakarta/nosql/keyvalue/package-info.java
@@ -22,10 +22,9 @@
  * once in the collection.
  * The key-value model is one of the simplest non-trivial data models, and richer data models are often implemented
  * as an extension of it.
- * The key-value model can be extended to a discretely ordered model that maintains keys in lexicographic order.
- * This extension is computationally powerful, in that it can efficiently retrieve selective key ranges.
- * Key-value stores can use consistency models ranging from eventual consistency to serializability.
- * Some databases support ordering of keys. There are various hardware implementations, and some users maintain
- * data in memory (RAM), while others employ solid-state drives or rotating disks.
+ * <p>
+ * The key-value model can be extended to a discretely ordered model that maintains keys in lexicographic order. This extension is computationally powerful, in that it can efficiently retrieve selective key ranges.
+ * <p>
+ * Key-value stores can use consistency models ranging from eventual consistency to serializability. Some databases support ordering of keys. There are various hardware implementations, and some users maintain data in memory (RAM), while others employ solid-state drives or rotating disks.
  */
 package jakarta.nosql.keyvalue;
