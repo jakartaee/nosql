222e2f31d90f7d0fe873befa06a85d6b8f4142e9
Otavio Santana <otaviopolianasantana@gmail.com>
2023-06-16 09:10:08 +0100

    doc: update javadoc at the core-module
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- api/nosql-core/src/main/java/module-info.java
+++ api/nosql-core/src/main/java/module-info.java
@@ -13,6 +13,14 @@
  *
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  */
+/**
+ * The Jakarta NoSQL Core.
+ * <p>
+ * The focus of this API is on simplicity and ease of use.
+ * Developers should only have to know a minimal set of
+ * artifacts to work with the solution.
+ * </p>
+ */
 module jakarta.nosql.core {
     exports jakarta.nosql;
 }
