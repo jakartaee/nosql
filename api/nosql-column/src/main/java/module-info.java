13033618737f4312ce4398777e96c7a818245727
Otavio Santana <otaviopolianasantana@gmail.com>
2023-06-16 10:36:38 +0100

    doc: update javadoc at the column-module
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- api/nosql-column/src/main/java/module-info.java
+++ api/nosql-column/src/main/java/module-info.java
@@ -13,6 +13,14 @@
  *
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  */
+/**
+ * The Jakarta NoSQL column module.
+ * <p>
+ * A column family is a NoSQL object that contains columns of related data. It is a tuple (pair)
+ * that consists of a key-value pair, where the key is mapped to a value that is a set of columns.
+ * In analogy with relational databases, a column family is as a <b>"table"</b>, each key-value pair being a <b>"row"</b>.
+ * </p>
+ */
 module jakarta.nosql.column {
     requires jakarta.nosql.core;
     exports jakarta.nosql.column;
