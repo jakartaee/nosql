4ec4d4dad56b5f5869a2e04d64618c5c432b561d
Otavio Santana <otaviopolianasantana@gmail.com>
2024-02-19 20:49:13 +0000

    docs: create discriminaotr value
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- api/nosql-core/src/main/java/jakarta/nosql/DiscriminatorValue.java
+++ api/nosql-core/src/main/java/jakarta/nosql/DiscriminatorValue.java
@@ -1,18 +1,19 @@
 /*
- *  Copyright (c) 2023 Contributors to the Eclipse Foundation
- *   All rights reserved. This program and the accompanying materials
- *   are made available under the terms of the Eclipse Public License v1.0
- *   and Apache License v2.0 which accompanies this distribution.
- *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
- *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ * Copyright (c) 2024 Contributors to the Eclipse Foundation
  *
- *   You may elect to redistribute this code under either of these licenses.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
  *
- *   Contributors:
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License v. 2.0 are satisfied: GNU General Public License, version 2
+ * with the GNU Classpath Exception which is available at
+ * https://www.gnu.org/software/classpath/license.html.
  *
- *   Otavio Santana
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  */
-package org.eclipse.jnosql.mapping;
+package jakarta.nosql;
 
 
 import java.lang.annotation.ElementType;
@@ -21,23 +22,23 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- *  Specifies the value of the discriminator column for entities of the given type.
+ * Specifies the value of the discriminator column for the annotated entity type.
+ * <p>The {@code DiscriminatorValue} annotation can only be specified on a concrete entity class.
+ * Specifies the value of the discriminator column for entities of the given type.
  *
- * <p> The <code>DiscriminatorValue</code>
- * annotation can only be specified on a concrete entity
- * class.
+ * <p>The <code>DiscriminatorValue</code> annotation can only be specified on a concrete entity class.
  *
- * <p> If the <code>DiscriminatorValue</code> annotation is not
- * specified and a discriminator column is used, a provider-specific
- * function will be used to generate a value representing the
- * entity type. So the discriminator value default is the {@link Class#getSimpleName()}.
+ * <p>If the <code>DiscriminatorValue</code> annotation is not specified and a discriminator column is used,
+ * a provider-specific function will be used to generate a value representing the entity type.
+ * So the discriminator value default is the {@link Class#getSimpleName()}.
+ * @see DiscriminatorColumn
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface DiscriminatorValue {
     /**
-     * (Optional) The value that indicates that the
-     * row is an entity of the annotated entity type.
+     * (Optional) The value that indicates that the row is an entity of the annotated entity type.
+     *
      * @return the discriminator Value
      */
     String value();
