416ab60ae4ad151d8cfd2229d1422611b2c96578
Otavio Santana <otaviopolianasantana@gmail.com>
2024-02-19 20:43:07 +0000

    docs: update inheritance annotation
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- api/nosql-core/src/main/java/jakarta/nosql/Inheritance.java
+++ api/nosql-core/src/main/java/jakarta/nosql/Inheritance.java
@@ -1,18 +1,19 @@
 /*
- *  Copyright (c) 2023 Contributors to the Eclipse Foundation
- *   All rights reserved. This program and the accompanying materials
- *   are made available under the terms of the Eclipse Public License v1.0
- *   and Apache License v2.0 which accompanies this distribution.
- *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
- *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ * Copyright (c) 2024 Contributors to the Eclipse Foundation
  *
- *   You may elect to redistribute this code under either of these licenses.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
  *
- *   Contributors:
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License v. 2.0 are satisfied: GNU General Public License, version 2
+ * with the GNU Classpath Exception which is available at
+ * https://www.gnu.org/software/classpath/license.html.
  *
- *   Otavio Santana
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  */
-package org.eclipse.jnosql.mapping;
+package jakarta.nosql;
 
 
 import java.lang.annotation.ElementType;
@@ -21,12 +22,13 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
+ * Specifies the inheritance mapping strategy for the entity class hierarchy, which descends from the annotated entity class.
  * Specifies the inheritance strategy to be used for an entity class hierarchy.
  * It is specified on the entity class that is the root of the entity class hierarchy.
  * This class can be either a regular or an abstract;
- * The table/column-family/document-collection will have a column for every attribute
+ * The NoSQL database structure will have a column for every attribute
  * of every class in the hierarchy.
- * The subclass will use the {@link jakarta.nosql.Entity} name from that class with this annotation.
+ * The subclass will use the {@link Entity} name from that class with this annotation.
  *
  * <pre>
  *
