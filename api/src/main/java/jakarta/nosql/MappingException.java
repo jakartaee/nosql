b5af02d5490569846c18cbf6173ab4cbf8673bfb
Otavio Santana <otaviopolianasantana@gmail.com>
2024-02-22 17:23:09 +0000

    docs: create exception at annotation
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- api/src/main/java/jakarta/nosql/MappingException.java
+++ api/src/main/java/jakarta/nosql/MappingException.java
@@ -16,35 +16,46 @@
 package jakarta.nosql;
 
 /**
- * An exception that occurs where there is a mapping error.
+ * An exception that occurs when there is a mapping error during entity mapping or persistence.
+ * This exception typically indicates issues with the usage or missing annotations at the entity level.
+ * Some scenarios where this exception may be thrown include:
+ * <ul>
+ *     <li>A class annotated with {@code @Entity} does not have the {@code @Id} annotation.</li>
+ *     <li>A class annotated with {@code @Entity} or {@code @Embeddable} does not have a default constructor
+ *     or has multiple constructors without {@code @Id} and {@code @Column} annotations.</li>
+ *     <li>A class annotated with {@code @Entity} or {@code @Embeddable} has multiple constructors with annotations
+ *     provided by Jakarta NoSQL, which may lead to ambiguity in entity instantiation.</li>
+ * </ul>
+ *
+ * <p>This exception may occur at runtime or build time, depending on the Jakarta NoSQL provider being used.</p>
  */
 public class MappingException extends NoSQLException {
 
     /**
-     * Constructs a {@code MappingException} using the given information.
+     * Constructs a {@code MappingException} using the given message and cause.
      *
      * @param message A message explaining the exception condition
-     * @param cause The underlying cause
+     * @param cause   The underlying cause
      */
     public MappingException(String message, Throwable cause) {
-        super( message, cause );
+        super(message, cause);
     }
 
     /**
-     * Constructs a {@code MappingException} using the given information.
+     * Constructs a {@code MappingException} using the given cause.
      *
      * @param cause The underlying cause
      */
     public MappingException(Throwable cause) {
-        super( cause );
+        super(cause);
     }
 
     /**
-     * Constructs a {@code MappingException} using the given information.
+     * Constructs a {@code MappingException} using the given message.
      *
      * @param message A message explaining the exception condition
      */
     public MappingException(String message) {
-        super( message );
+        super(message);
     }
 }
