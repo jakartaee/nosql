b3748053ff455acd81faadf5244d6863dd4b91d6
Otavio Santana <otaviopolianasantana@gmail.com>
2024-12-24 10:53:33 +0000

    feat: create initial structure to query
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- tck/src/main/java/jakarta/nosql/tck/QueryTemplateInheritanceTest.java
+++ tck/src/main/java/jakarta/nosql/tck/QueryTemplateInheritanceTest.java
@@ -1,4 +1,24 @@
+/*
+ * Copyright (c) 2024 Contributors to the Eclipse Foundation
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ */
 package jakarta.nosql.tck;
+
+import jakarta.nosql.tck.entities.Animal;
+import jakarta.nosql.tck.factories.AnimalListSupplier;
+import org.assertj.core.api.Assertions;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+
+import java.util.List;
 
 public class QueryTemplateInheritanceTest {
 }
