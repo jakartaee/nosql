8fca1a71e791c9c2fe6731a2a7606ed346ca1c1f
Otavio Santana <otaviopolianasantana@gmail.com>
2024-12-24 11:27:54 +0000

    feat: update header
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- tck/src/main/java/jakarta/nosql/tck/entities/Drink.java
+++ tck/src/main/java/jakarta/nosql/tck/entities/Drink.java
@@ -15,19 +15,5 @@
  */
 package jakarta.nosql.tck.entities;
 
-import jakarta.nosql.Column;
-import jakarta.nosql.Entity;
-import jakarta.nosql.Id;
-import net.datafaker.Faker;
-
-import java.util.UUID;
-
-@Entity
-public record Book(@Id String id, @Column String title, @Column String author, @Column String publisher,
-                   @Column String genre) {
-
-    public static Book of(Faker faker){
-        var book = faker.book();
-        return new Book(UUID.randomUUID().toString(), book.title(), book.author(), book.publisher(), book.genre());
-    }
+public class Drink {
 }
