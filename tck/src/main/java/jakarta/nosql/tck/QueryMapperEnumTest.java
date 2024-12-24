6a7dd1b20389670b2de9c49ead17784f0ef9610e
Otavio Santana <otaviopolianasantana@gmail.com>
2024-12-24 09:55:55 +0000

    feat: update the structure to enum value
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ tck/src/main/java/jakarta/nosql/tck/QueryMapperEnumTest.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2024 Contributors to the Eclipse Foundation
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License v. 2.0 are satisfied: GNU General Public License, version 2
+ * with the GNU Classpath Exception which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ */
+package jakarta.nosql.tck;
+
+
+import jakarta.nosql.tck.entities.Vehicle;
+import jakarta.nosql.tck.factories.VehicleListSupplier;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+
+import java.util.List;
+
+public class QueryMapperEnumTest extends AbstractTemplateTest {
+
+}
