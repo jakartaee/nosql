41ec497d8ab63e1b6d8a4aab99ae699c027d0f3d
Otavio Santana <otaviopolianasantana@gmail.com>
2024-12-24 09:50:00 +0000

    chore: create structure to delete
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ tck/src/main/java/jakarta/nosql/tck/QueryMapperDeleteTemplateTest.java
@@ -0,0 +1,14 @@
+package jakarta.nosql.tck;
+
+import jakarta.nosql.tck.entities.Person;
+import jakarta.nosql.tck.factories.PersonListSupplier;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+
+import java.util.List;
+
+public class QueryMapperDeleteTemplateTest extends AbstractTemplateTest {
+}
