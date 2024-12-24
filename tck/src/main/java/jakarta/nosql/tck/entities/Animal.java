93b28415a7e87ed31b7fb4eb2af209b74606b6a0
Otavio Santana <otaviopolianasantana@gmail.com>
2024-12-24 10:31:00 +0000

    feat: create animal entity
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- tck/src/main/java/jakarta/nosql/tck/entities/Animal.java
+++ tck/src/main/java/jakarta/nosql/tck/entities/Animal.java
@@ -1,4 +1,15 @@
 package jakarta.nosql.tck.entities;
 
-public class Animal {
+import jakarta.nosql.Entity;
+
+@Entity
+public class Animal extends AbstractAnimal {
+
+private String name;
+
+private String scientificName;
+
+private String genus;
+
+private String species;
 }
