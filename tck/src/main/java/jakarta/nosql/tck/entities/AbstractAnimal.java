510699536c5e9754657f0cad5a2a9f52234c37aa
Otavio Santana <otaviopolianasantana@gmail.com>
2024-12-24 10:32:23 +0000

    feat: create and implement equals and hashcode
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- tck/src/main/java/jakarta/nosql/tck/entities/AbstractAnimal.java
+++ tck/src/main/java/jakarta/nosql/tck/entities/AbstractAnimal.java
@@ -3,9 +3,25 @@ package jakarta.nosql.tck.entities;
 import jakarta.nosql.Id;
 import jakarta.nosql.MappedSuperclass;
 
+import java.util.Objects;
+
 @MappedSuperclass
 public abstract class AbstractAnimal {
 
     @Id
     protected String id;
+
+
+    @Override
+    public boolean equals(Object object) {
+        if (!(object instanceof AbstractAnimal that)) {
+            return false;
+        }
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
