a9b65fe34c8fb800fc6755bc07d653430e8ae2bb
Otavio Santana <otaviopolianasantana@gmail.com>
2024-12-25 07:32:26 +0000

    style: remove unsed imports at QueryMapperDeleteRecord
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- tck/src/main/java/jakarta/nosql/tck/QueryMapperDeleteRecordTest.java
+++ tck/src/main/java/jakarta/nosql/tck/QueryMapperDeleteRecordTest.java
@@ -11,7 +11,6 @@ package jakarta.nosql.tck;
 
 import jakarta.nosql.tck.entities.Book;
 import jakarta.nosql.tck.factories.BookListSupplier;
-import jakarta.nosql.tck.factories.BookSupplier;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
