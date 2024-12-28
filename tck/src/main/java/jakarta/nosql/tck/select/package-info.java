a470e802be9d7a34dcf55e2c5ec48a0398513bd5
Otavio Santana <otaviopolianasantana@gmail.com>
2024-12-28 06:42:16 +0000

    feat: create package info
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ tck/src/main/java/jakarta/nosql/tck/select/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2024 Contributors to the Eclipse Foundation
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ */
+
+package jakarta.nosql.tck.select;
