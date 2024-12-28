ae860c1fda3978d4dd40d354e7a8c8ba8efc6007
Otavio Santana <otaviopolianasantana@gmail.com>
2024-12-28 19:55:41 +0000

    feat: create structure to basicOperation
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ tck/src/main/java/jakarta/nosql/tck/basic/BasicOperationsWithInheritanceTest.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2024 Contributors to the Eclipse Foundation
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License v. 2.0 are satisfied: GNU General Public License, version 2
+ * with the GNU Classpath Exception which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ */
+package jakarta.nosql.tck.basic;
+
+import jakarta.nosql.tck.AbstractTemplateTest;
+import jakarta.nosql.tck.entities.Beer;
+import jakarta.nosql.tck.entities.Coffee;
+import jakarta.nosql.tck.entities.Drink;
+import jakarta.nosql.tck.factories.DrinkSupplier;
+import org.assertj.core.api.Assertions;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+
+import java.time.Duration;
+import java.util.logging.Logger;
+
+public class BasicOperationsWithInheritanceTest {
+}
