c3dd08758e8c8897abc3abfe53cf24e52009f16e
Otavio Santana <otaviopolianasantana@gmail.com>
2024-12-28 19:49:27 +0000

    feat: create drink list supplier
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ tck/src/main/java/jakarta/nosql/tck/factories/DrinkListSupplier.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2024 Contributors to the Eclipse Foundation
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License v. 2.0 are satisfied: GNU General Public License, version 2
+ * with the GNU Classpath Exception which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ */
+package jakarta.nosql.tck.factories;
+
+import jakarta.nosql.tck.entities.Drink;
+import jakarta.nosql.tck.entities.Vehicle;
+
+public class DrinkListSupplier extends AbstractListSupplier<Drink> {
+
+    @Override
+    Drink getEntity() {
+        return Vehicle.of(faker());
+    }
+}
