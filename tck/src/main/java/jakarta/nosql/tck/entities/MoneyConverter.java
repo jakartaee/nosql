ad75a96068bba0b3a135562a2522cb5c005c1c70
Otavio Santana <otaviopolianasantana@gmail.com>
2025-01-18 06:43:03 +0000

    feat: create money converter
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- tck/src/main/java/jakarta/nosql/tck/entities/MoneyConverter.java
+++ tck/src/main/java/jakarta/nosql/tck/entities/MoneyConverter.java
@@ -1,16 +1,42 @@
+/*
+ * Copyright (c) 2025 Contributors to the Eclipse Foundation
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License v. 2.0 are satisfied: GNU General Public License, version 2
+ * with the GNU Classpath Exception which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ */
 package jakarta.nosql.tck.entities;
 
 import jakarta.nosql.AttributeConverter;
+
+import java.math.BigDecimal;
+import java.util.Currency;
 
 public class MoneyConverter implements AttributeConverter<Money, String> {
 
     @Override
     public String convertToDatabaseColumn(Money attribute) {
-        return "";
+
+        if (attribute == null) {
+            return null;
+        }
+        return attribute.toString();
     }
 
     @Override
     public Money convertToEntityAttribute(String dbData) {
-        return null;
+        if (dbData == null) {
+            return null;
+        }
+        String[] values = dbData.split(" ");
+        return new Money(Currency.getInstance(values[0]), new BigDecimal(values[1]));
     }
 }
