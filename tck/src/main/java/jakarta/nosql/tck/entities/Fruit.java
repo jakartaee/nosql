0a57aa7eafc29d3640ac0bc9cffd5c13aefa04f5
Otavio Santana <otaviopolianasantana@gmail.com>
2025-01-18 06:47:52 +0000

    feat: create fruit structure
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- tck/src/main/java/jakarta/nosql/tck/entities/Fruit.java
+++ tck/src/main/java/jakarta/nosql/tck/entities/Fruit.java
@@ -34,5 +34,29 @@ public class Fruit {
     @Convert(MoneyConverter.class)
     private Money price;
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Money getPrice() {
+        return price;
+    }
+
+    public void setPrice(Money price) {
+        this.price = price;
+    }
+
 
 }
