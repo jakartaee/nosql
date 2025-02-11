1b25bbd51cdb02de17b854367aa395f722ee647d
Otavio Santana <otaviopolianasantana@gmail.com>
2025-02-11 08:19:49 +0000

    docs: enhance documentation at entties
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ tck/src/main/java/ee/jakarta/tck/nosql/entities/package-info.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2024 Contributors to the Eclipse Foundation
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ */
+
+/**
+ * This package contains entity classes and converters for use in Jakarta NoSQL TCK tests.
+ * The entities represent domain models used to validate Jakarta NoSQL implementations,
+ * while converters handle type transformations where necessary.
+ * These classes support testing CRUD operations, queries, and annotation behavior
+ * across different NoSQL databases.
+ */
+package ee.jakarta.tck.nosql.entities;
