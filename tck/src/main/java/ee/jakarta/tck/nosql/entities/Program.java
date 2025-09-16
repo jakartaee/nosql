63708d601eb97c4dff418e9dda7077a69ca57a63
Otavio Santana <otaviopolianasantana@gmail.com>
2025-09-16 04:35:31 +0100

    test: add Computer and Program classes with appropriate annotations

--- tck/src/main/java/ee/jakarta/tck/nosql/entities/Program.java
+++ tck/src/main/java/ee/jakarta/tck/nosql/entities/Program.java
@@ -1,4 +1,65 @@
+/*
+ * Copyright (c) 2025 Contributors to the Eclipse Foundation
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License v. 2.0 are satisfied: GNU General Public License, version 2
+ * with the GNU Classpath Exception which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ */
 package ee.jakarta.tck.nosql.entities;
 
+
+import jakarta.nosql.Column;
+import jakarta.nosql.Entity;
+import jakarta.nosql.Id;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.Objects;
+
+@Entity
 public class Program {
+
+    @Id
+    private String name;
+
+    @Column
+    private Map<String, Program> programs;
+
+    public String getName() {
+        return name;
+    }
+
+    public Map<String, Program> getPrograms() {
+        return Collections.unmodifiableMap(programs);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Program program = (Program) o;
+        return Objects.equals(name, program.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
+    }
+
+    @Override
+    public String toString() {
+        return "Program{" +
+                "name='" + name + '\'' +
+                ", programs=" + programs +
+                '}';
+    }
 }
