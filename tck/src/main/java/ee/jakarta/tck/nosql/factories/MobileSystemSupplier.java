0e57134886dadd0ce370ce0dc09ae0a3eb17babd
Otavio Santana <otaviopolianasantana@gmail.com>
2025-09-16 07:32:10 +0100

    test: add MobileSystemSupplier for generating MobileApp instances

---
+++ tck/src/main/java/ee/jakarta/tck/nosql/factories/MobileSystemSupplier.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2025 Contributors to the Eclipse Foundation
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License v. 2.0 are satisfied: GNU General Public License, version 2
+ * with the GNU Classpath Exception which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ */
+package ee.jakarta.tck.nosql.factories;
+
+import ee.jakarta.tck.nosql.entities.MobileApp;
+import ee.jakarta.tck.nosql.entities.Program;
+import net.datafaker.Faker;
+
+import java.util.Map;
+import java.util.UUID;
+
+public class MobileSystemSupplier extends AbstractSupplier<MobileApp>  {
+
+    @Override
+    public MobileApp get() {
+        Faker faker = faker();
+        var id = UUID.randomUUID().toString();
+        var program = Program.of(faker.app().name(), Map.of("version", faker.app().version(),
+                "language", faker.programmingLanguage().name()));
+        var program2 = Program.of(faker.app().name(), Map.of("version", faker.app().version(),
+                "language", faker.programmingLanguage().name()));
+        var programs = Map.of("programA", program, "programB", program2);
+
+        return new MobileApp(id, programs);
+    }
+}
