41c0cc959b48af2b98291cfb692021aedfeb090b
Otavio Santana <otaviopolianasantana@gmail.com>
2026-01-02 18:57:41 +0000

    test: add parameterized test for 'lte' scenario in SelectFromWhereTest

--- tck/src/main/java/ee/jakarta/tck/nosql/query/SelectFromWhereTest.java
+++ tck/src/main/java/ee/jakarta/tck/nosql/query/SelectFromWhereTest.java
@@ -234,11 +234,18 @@ public class SelectFromWhereTest extends AbstractTemplateTest {
                     .allMatch(fruit -> fruit.getQuantity() < sample.getQuantity());
         }
 
+        @ParameterizedTest
+        @DisplayName("should test lte")
+        @ArgumentsSource(FruitListSupplier.class)
+        void shouldLte(List<Fruit> fruits) {
+            template.insert(fruits);
+            Fruit sample = fruits.getFirst();
+            List<Fruit> result = template.typedQuery("FROM Fruit WHERE quantity <= " + sample.getQuantity(), Fruit.class)
+                    .result();
+
+            assertThat(result)
+                    .isNotEmpty()
+                    .allMatch(fruit -> fruit.getQuantity() <= sample.getQuantity());
+        }
     }
-
-    //should lt
-    //should lte
-    //should like
-    //should in
-
 }
