69bb629d85aecea3777b587f04497b318eef49a6
Otavio Santana <otaviopolianasantana@gmail.com>
2026-01-10 20:47:18 +0000

    test: add parameterized test for updating entities in UpdateFromTest

--- tck/src/main/java/ee/jakarta/tck/nosql/query/UpdateFromTest.java
+++ tck/src/main/java/ee/jakarta/tck/nosql/query/UpdateFromTest.java
@@ -17,9 +17,29 @@ package ee.jakarta.tck.nosql.query;
 
 
 import ee.jakarta.tck.nosql.AbstractTemplateTest;
+import ee.jakarta.tck.nosql.entities.Fruit;
+import ee.jakarta.tck.nosql.factories.FruitListSupplier;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+
+import java.util.List;
 
 @DisplayName("The Jakarta Query integration test using update")
 class UpdateFromTest extends AbstractTemplateTest {
 
+    @ParameterizedTest
+    @DisplayName("should delete all entities")
+    @ArgumentsSource(FruitListSupplier.class)
+    void shouldUpdateEntities(List<Fruit> fruits) {
+        try {
+            template.insert(fruits);
+            template.query("UPDATE Fruit SET quantity = 19").executeUpdate();
+            List<Fruit> result = template.query("FROM Fruit").result();
+            Assertions.assertThat(result).isNotEmpty().allMatch(fruit -> fruit.getQuantity() == 19);
+        } catch (UnsupportedOperationException exp) {
+            Assertions.assertThat(exp).isInstanceOf(UnsupportedOperationException.class);
+        }
+    }
 }
