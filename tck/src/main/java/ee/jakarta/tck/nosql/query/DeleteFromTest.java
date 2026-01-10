584db5d3b2d9576a9c9126a5a47a98cb7f70ad72
Otavio Santana <otaviopolianasantana@gmail.com>
2026-01-10 20:20:43 +0000

    test: add parameterized tests for AND and OR queries in DeleteFromTest

--- tck/src/main/java/ee/jakarta/tck/nosql/query/DeleteFromTest.java
+++ tck/src/main/java/ee/jakarta/tck/nosql/query/DeleteFromTest.java
@@ -188,4 +188,51 @@ class DeleteFromTest extends AbstractTemplateTest {
         }
     }
 
+    @ParameterizedTest
+    @DisplayName("should test AND")
+    @ArgumentsSource(FruitListSupplier.class)
+    void shouldAnd(List<Fruit> fruits) {
+
+        try {
+            template.insert(fruits);
+            Fruit sample = fruits.getFirst();
+            List<Fruit> result = template.typedQuery("FROM Fruit WHERE name = :name AND quantity " +
+                            "= :quantity", Fruit.class)
+                    .bind("name", sample.getName())
+                    .bind("quantity", sample.getQuantity())
+                    .result();
+
+            AssertionsForInterfaceTypes.assertThat(result)
+                    .isNotEmpty()
+                    .allMatch(fruit -> fruit.getName().equals(sample.getName())
+                            && fruit.getQuantity().equals(sample.getQuantity()));
+        } catch (UnsupportedOperationException exp) {
+            Assertions.assertThat(exp).isInstanceOf(UnsupportedOperationException.class);
+        }
+    }
+
+    @ParameterizedTest
+    @DisplayName("should test OR")
+    @ArgumentsSource(FruitListSupplier.class)
+    void shouldOr(List<Fruit> fruits) {
+
+        try {
+            template.insert(fruits);
+            Fruit sample1 = fruits.get(0);
+            Fruit sample2 = fruits.get(1);
+            List<Fruit> result = template.typedQuery("FROM Fruit WHERE name = :name1 OR name = :name2", Fruit.class)
+                    .bind("name1", sample1.getName())
+                    .bind("name2", sample2.getName())
+                    .result();
+
+            AssertionsForInterfaceTypes.assertThat(result)
+                    .isNotEmpty()
+                    .allMatch(fruit -> fruit.getName().equals(sample1.getName())
+                            || fruit.getName().equals(sample2.getName()));
+
+        } catch (UnsupportedOperationException exp) {
+            Assertions.assertThat(exp).isInstanceOf(UnsupportedOperationException.class);
+        }
+    }
+
 }
