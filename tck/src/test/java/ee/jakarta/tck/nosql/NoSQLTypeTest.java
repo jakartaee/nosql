ada476ed3a3213706fb0cd3da53a3e6e9164ef46
Otavio Santana <otaviopolianasantana@gmail.com>
2026-09-01 07:49:51 +0100

    test: reorganize `NoSQLTypeTest` with nested tests and improved structure

--- tck/src/test/java/ee/jakarta/tck/nosql/NoSQLTypeTest.java
+++ tck/src/test/java/ee/jakarta/tck/nosql/NoSQLTypeTest.java
@@ -15,59 +15,148 @@
  */
 package ee.jakarta.tck.nosql;
 
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatNullPointerException;
+
 class NoSQLTypeTest {
 
-    @ParameterizedTest(name = "{0} should have a flexibility level of {1}")
-    @CsvSource({
-            "KEY_VALUE, 1",
-            "COLUMN, 2",
-            "DOCUMENT, 3",
-            "GRAPH, 4",
-            "OTHER, 0"
-    })
-    public void shouldReturnCorrectFlexibility(String type, int expectedFlexibility) {
-        Assertions.assertThat(NoSQLType.valueOf(type).getFlexibility()).isEqualTo(expectedFlexibility);
-    }
-
-    @ParameterizedTest(name = "get({0}) should return {1}")
-    @CsvSource({
-            "COLUMN, COLUMN",
-            "GRAPH, GRAPH",
-            "KEY_VALUE, KEY_VALUE"
-    })
-    public void shouldReturnTypeFromValidString(String input, String expectedType) {
-        Assertions.assertThat(NoSQLType.get(input)).isEqualTo(NoSQLType.valueOf(expectedType));
-    }
-
-    @ParameterizedTest(name = "get({0}) should return the default type KEY_VALUE for invalid input")
-    @ValueSource(strings = {"INVALID_TYPE", "UNKNOWN", ""})
-    public void shouldReturnDefaultTypeForInvalidString(String input) {
-        Assertions.assertThat(NoSQLType.get(input)).isEqualTo(NoSQLType.KEY_VALUE);
-    }
-
-    @Test
-    public void shouldReturnTypeFromValidSystemProperty() {
-        System.setProperty(NoSQLType.DATABASE_TYPE_PROPERTY, "COLUMN");
-        Assertions.assertThat(NoSQLType.get()).isEqualTo(NoSQLType.COLUMN);
+    @AfterEach
+    void clearDatabaseTypeProperty() {
         System.clearProperty(NoSQLType.DATABASE_TYPE_PROPERTY);
     }
 
-    @Test
-    public void shouldReturnDefaultTypeForInvalidSystemProperty() {
-        System.setProperty(NoSQLType.DATABASE_TYPE_PROPERTY, "INVALID_TYPE");
-        Assertions.assertThat(NoSQLType.get()).isEqualTo(NoSQLType.KEY_VALUE);
-        System.clearProperty(NoSQLType.DATABASE_TYPE_PROPERTY);
+    @Nested
+    @DisplayName("When reading database type flexibility")
+    class WhenTheFlexibilityIsRead {
+
+        @ParameterizedTest(name = "{0} should have flexibility level {1}")
+        @CsvSource({
+                "KEY_VALUE, 1",
+                "COLUMN, 2",
+                "DOCUMENT, 3",
+                "GRAPH, 4",
+                "OTHER, 0"
+        })
+        @DisplayName("Should return the configured level")
+        void shouldReturnTheConfiguredLevel(String type, int expectedFlexibility) {
+
+            // Given
+            var noSQLType = NoSQLType.valueOf(type);
+
+            // When
+            var flexibility = noSQLType.getFlexibility();
+
+            // Then
+            assertThat(flexibility)
+                    .as("flexibility level for %s", type)
+                    .isEqualTo(expectedFlexibility);
+        }
     }
 
-    @Test
-    public void shouldReturnDefaultTypeWhenNoSystemPropertyIsSet() {
-        System.clearProperty(NoSQLType.DATABASE_TYPE_PROPERTY);
-        Assertions.assertThat(NoSQLType.get()).isEqualTo(NoSQLType.KEY_VALUE);
+    @Nested
+    @DisplayName("When resolving a database type by name")
+    class WhenTheTypeIsResolvedByName {
+
+        @ParameterizedTest(name = "{0} should resolve to {1}")
+        @CsvSource({
+                "COLUMN, COLUMN",
+                "document, DOCUMENT",
+                "Graph, GRAPH",
+                "KEY_VALUE, KEY_VALUE",
+                "OTHER, OTHER"
+        })
+        @DisplayName("Should return the matching type")
+        void shouldReturnTheMatchingType(String input, NoSQLType expectedType) {
+
+            // When
+            var type = NoSQLType.get(input);
+
+            // Then
+            assertThat(type)
+                    .as("database type resolved from %s", input)
+                    .isEqualTo(expectedType);
+        }
+
+        @ParameterizedTest(name = "Invalid value \"{0}\" should resolve to KEY_VALUE")
+        @ValueSource(strings = {"INVALID_TYPE", "UNKNOWN", ""})
+        @DisplayName("Should return the default type when the name is invalid")
+        void shouldReturnTheDefaultTypeWhenTheNameIsInvalid(String input) {
+
+            // When
+            var type = NoSQLType.get(input);
+
+            // Then
+            assertThat(type)
+                    .as("default database type for invalid name")
+                    .isEqualTo(NoSQLType.KEY_VALUE);
+        }
+
+        @Test
+        @DisplayName("Should reject a null name")
+        void shouldRejectANullName() {
+
+            // When / Then
+            assertThatNullPointerException()
+                    .as("null database type name")
+                    .isThrownBy(() -> NoSQLType.get(null));
+        }
+    }
+
+    @Nested
+    @DisplayName("When resolving a database type from the system property")
+    class WhenTheTypeIsResolvedFromTheSystemProperty {
+
+        @Test
+        @DisplayName("Should return the configured type")
+        void shouldReturnTheConfiguredType() {
+
+            // Given
+            System.setProperty(NoSQLType.DATABASE_TYPE_PROPERTY, "COLUMN");
+
+            // When
+            var type = NoSQLType.get();
+
+            // Then
+            assertThat(type)
+                    .as("database type from the system property")
+                    .isEqualTo(NoSQLType.COLUMN);
+        }
+
+        @Test
+        @DisplayName("Should return the default type when the configured value is invalid")
+        void shouldReturnTheDefaultTypeWhenTheConfiguredValueIsInvalid() {
+
+            // Given
+            System.setProperty(NoSQLType.DATABASE_TYPE_PROPERTY, "INVALID_TYPE");
+
+            // When
+            var type = NoSQLType.get();
+
+            // Then
+            assertThat(type)
+                    .as("default database type for an invalid system property")
+                    .isEqualTo(NoSQLType.KEY_VALUE);
+        }
+
+        @Test
+        @DisplayName("Should return the default type when the property is absent")
+        void shouldReturnTheDefaultTypeWhenThePropertyIsAbsent() {
+
+            // When
+            var type = NoSQLType.get();
+
+            // Then
+            assertThat(type)
+                    .as("default database type without a system property")
+                    .isEqualTo(NoSQLType.KEY_VALUE);
+        }
     }
 }
